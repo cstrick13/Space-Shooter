5d7e8a8651773064d5109b6152c865b1c10e04d3
Christian Salassi <salassichristian@gmail.com>
2025-03-18 16:58:42 -0500

    Crude Enemy Shooter
    Implemented the shooting functionality to the enemy "shooter" type enemy. Fires at changeable intervals towards the left of the screen. DEBUG key to spawn them is " ' ".

--- Assets/Scripts/ShootingEnemy.cs
+++ Assets/Scripts/ShootingEnemy.cs
@@ -7,10 +7,15 @@ public class ShootingEnemy : MonoBehaviour
     public GameObject target;
     public float moveSpeed;
     public float dFromPlayer;
+    public GameObject bulletPrefab;
+    private int timer = 0;
+    public Transform spawnPt;
+    public int delayShoot = 100;
     // Start is called before the first frame update
     void Start()
     {
         target = GameObject.FindWithTag("Player");
+        //bulletPrefab = Resources.Load<GameObject>("Prefabs/EnemyBullet");
         dFromPlayer = 7.5f;
         moveSpeed = 2;
     }
@@ -20,11 +25,23 @@ public class ShootingEnemy : MonoBehaviour
     {
         //Find the point x amount in front of players current position to move towards.
         Vector3 align = new Vector3((target.transform.position.x+dFromPlayer),target.transform.position.y,target.transform.position.z);
-        print(target.transform.position);
         if (target.transform.position != this.transform.position)
         {
             transform.position = Vector3.MoveTowards(transform.position, align, moveSpeed * Time.deltaTime);
-            print((transform.position.x,transform.position.y,0));
+        }
+        if (timer > delayShoot)
+        {
+            if (bulletPrefab != null)
+            {
+                print("shooting!");
+                var bullet = Instantiate(bulletPrefab);
+                bullet.transform.position = spawnPt.position;
+            }
+            timer = 0;
+        }
+        else
+        {
+            timer += 1;
         }
     }
 }
