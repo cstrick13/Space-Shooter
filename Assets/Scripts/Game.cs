64ac50a1c65c6657b9722224799fafd0fb505180
Cory Strickland <corystrickland13@gmail.com>
2025-03-18 22:09:30 -0400

    Spawn Different Enemy Types
    This is the logic for spawning enemy types

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,21 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using Random = UnityEngine.Random;
 
 public class Game : MonoBehaviour
 {
     //public bool DEBUG;
+    public GameObject enemyPrefab;
+    public GameObject droneEnemyPrefab;
+
+    public GameObject bulletEnemyPrefab;
+    public float enemyTimer;
     public static GameControls Input { get; private set;}
     // Start is called before the first frame update
     void Start()
     {
         Input = new GameControls();
         Input.Enable();
+        enemyTimer = 3f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyTimer -= Time.deltaTime;
+        if(enemyTimer < 0){
+            Instantiate(enemyPrefab);
+            Instantiate(droneEnemyPrefab);
+            Instantiate(bulletEnemyPrefab);
+            enemyTimer = Random.Range(2f,7f);
+        }
     }
 }
